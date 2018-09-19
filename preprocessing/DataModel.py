2b33a9cee87654acc4602f34cf326b0a84127d3f
rezaasad <rayza.asad@gmail.com>
2018-09-18 19:50:06 -0700

    Creating the structure of the project. Initialized the DataModel.

---
+++ preprocessing/DataModel.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+
+class LFWDataset(Dataset):
+    def __init__(self, dataList):
+        self.dataList = dataList
+
+    def __len__(self):
+        return len(self.dataList)
+
+
