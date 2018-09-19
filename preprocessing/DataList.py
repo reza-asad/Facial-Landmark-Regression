56f9a85532736f9be2533dd43ee28cab77bd229a
rezaasad <rayza.asad@gmail.com>
2018-09-18 20:36:49 -0700

    Added basic set of operations for making the data list.

--- preprocessing/DataList.py
+++ preprocessing/DataList.py
@@ -5,20 +5,44 @@ class DataList():
         self.labelPath = labelPath
         self.imagePath = imagePath
 
-    def ScaleImg(self):
+    def __ToTensor(self, img):
         pass
 
-    def OpenImg(self):
+    def __ScaleImg(self, img):
+        pass
+
+    def __OpenImg(self, imgName):
         pass
 
     def __Crop(self, img):
         pass
 
     def __MakeList(self):
-        pass
+        with open(labelPath, 'r'):
+            for line in f:
+                # Extract the label
+                label = line.split()
+                # Extract the landmarks
+                landMarks = np.array(label[])
+                # Open the image
+                img = self.__OpenImg(label[0])
+                # Crop the image
+                img = self.__Crop(img)
+                # Scale the image to (-1, 1)
+                img = self.__ScaleImg(img)
+                # Similarly scale the landmarks to (-1, 1)
+                landMarks = self.__ScaleImg(landMarks)
+                # Create a tensor object from the image
+                imgTensor = self.__ToTensor(img)
+                # Create tensor labels.
+                labelTensor = self.__ToTensor(landMarks).long()
+                # Add the data point to the data list.
+                self.data.append({'label':labelTensor, 'img':imgTensor})
+
+
 
 
 # small test
-labelPath = '/Users/rezaasad/Documents/CMPT742/Project01/data/training_data'
+labelPath = '/Users/rezaasad/Documents/CMPT742/Project01/data/training_data/LFW_annotation_train.txt'
 imagePath = '/Users/rezaasad/Documents/CMPT742/Project01/data/lfw'
 d = DataList(labelPath, imagePath)
